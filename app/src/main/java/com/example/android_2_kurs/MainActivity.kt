0d9e9be7d6eb4a800127fc4c2a11cf36352b6be1
Alexey <dyakonov128@gmail.com>
2020-09-20 23:58:08 +0300

    myActivity

--- app/src/main/java/com/example/android_2_kurs/MainActivity.kt
+++ app/src/main/java/com/example/android_2_kurs/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.android_2_kurs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editNameButton.setOnClickListener {
+
+            if (nameUser.visibility == View.VISIBLE) {
+                nameUser.setVisibility(View.INVISIBLE)
+                id_editNameText.setVisibility(View.VISIBLE)
+                id_editNameText.setText(nameUser.text)
+            } else {
+                nameUser.setVisibility(View.VISIBLE)
+                id_editNameText.setVisibility(View.INVISIBLE)
+                nameUser.text = id_editNameText.text
+            }
+        }
     }
 }
