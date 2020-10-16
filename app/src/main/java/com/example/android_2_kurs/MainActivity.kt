0920b6460f4fc624e0ece093f941184946db1bb3
Alexey <dyakonov128@gmail.com>
2020-10-16 08:30:40 +0300

    Home_4

--- app/src/main/java/com/example/android_2_kurs/MainActivity.kt
+++ app/src/main/java/com/example/android_2_kurs/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.example.android_2_kurs
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.id_frameLayout, FragmentThree.newInstance("1", "1"))
+            .commit()
+
+        imageView1.setOnClickListener {
+            resetColors(imageView1)
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fade_in, R.anim.slide_out)
+                .replace(R.id.id_frameLayout, FragmentOne.newInstance("1", "1")).commit()
+
+        }
+
+        imageView2.setOnClickListener {
+            resetColors(imageView2)
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fade_in, R.anim.slide_out)
+                .replace(R.id.id_frameLayout, FragmentTwo.newInstance("1", "1")).commit()
+
+        }
+
+        imageView3.setOnClickListener {
+            resetColors(imageView3)
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fade_in, R.anim.slide_out)
+                .replace(R.id.id_frameLayout, FragmentThree.newInstance("1", "1")).commit()
+
+        }
+
+        imageView4.setOnClickListener {
+            resetColors(imageView4)
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fade_in, R.anim.slide_out)
+                .replace(R.id.id_frameLayout, FragmentFour.newInstance("1", "1")).commit()
+
+        }
+
+        imageView5.setOnClickListener {
+            resetColors(imageView5)
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fade_in, R.anim.slide_out)
+                .replace(R.id.id_frameLayout, FragmentFive.newInstance("1", "1")).commit()
+
+        }
+
+    }
+
+    fun resetColors(a: ImageView){
+        imageView1.isSelected = false
+        imageView2.isSelected = false
+        imageView3.isSelected = false
+        imageView4.isSelected = false
+        imageView5.isSelected = false
+        a.isSelected = true
+
     }
 }
